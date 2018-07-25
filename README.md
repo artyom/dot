bf189b6da4d09c5e9c5e20aef40df7ba7365daf2
Artyom Pervukhin <artyom@evasive.ru>
2018-07-25 20:25:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Package dot provides some known DNS-over-TLS (DOT) resolvers.
+
+See documentation for details: <https://godoc.org/github.com/artyom/dot>
---
+++ dot.go
@@ -0,0 +1,52 @@
+// Package dot provides some known DNS-over-TLS (DOT) resolvers.
+package dot
+
+import (
+	"context"
+	"crypto/tls"
+	"math/rand"
+	"net"
+	"time"
+)
+
+// Cloudflare returns Resolver that uses Cloudflare service on 1.1.1.1 and
+// 1.0.0.1 on port 853.
+//
+// See https://developers.cloudflare.com/1.1.1.1/dns-over-tls/ for details.
+func Cloudflare() *net.Resolver {
+	return newResolver("cloudflare-dns.com", "1.1.1.1:853", "1.0.0.1:853")
+}
+
+// Quad9 returns Resolver that uses Quad9 service on 9.9.9.9 and 149.112.112.112
+// on port 853.
+//
+// See https://quad9.net/faq/ for details.
+func Quad9() *net.Resolver {
+	return newResolver("dns.quad9.net", "9.9.9.9:853", "149.112.112.112:853")
+}
+
+func newResolver(serverName string, addrs ...string) *net.Resolver {
+	if serverName == "" {
+		panic("dot: server name cannot be empty")
+	}
+	if len(addrs) == 0 {
+		panic("dot: addrs cannot be empty")
+	}
+	var d net.Dialer
+	cfg := &tls.Config{
+		ServerName:         serverName,
+		ClientSessionCache: tls.NewLRUClientSessionCache(0),
+	}
+	return &net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			conn, err := d.DialContext(ctx, "tcp", addrs[rand.Intn(len(addrs))])
+			if err != nil {
+				return nil, err
+			}
+			conn.(*net.TCPConn).SetKeepAlive(true)
+			conn.(*net.TCPConn).SetKeepAlivePeriod(3 * time.Minute)
+			return tls.Client(conn, cfg), nil
+		},
+	}
+}
